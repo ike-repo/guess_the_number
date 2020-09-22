301295ca5aa71aabc7c774506a6b48b804830547
ike-repo <ikeolg@gmail.com>
2020-09-21 21:15:44 -0400

    add js and styling

---
+++ script.js
@@ -0,0 +1,46 @@
+let numToFind = Math.floor((Math.random() * 100) + 1);
+
+document.getElementById("button").addEventListener("click", findNumber);
+document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("input").value = ""
+    document.getElementById("input").focus();
+});
+document.getElementById("input").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("button").click();
+    }
+    if (event.keyCode === 13) {
+        document.getElementById("input").value = "";
+    }
+});
+
+
+let count = 0;
+count++;
+
+function findNumber() {
+    const guess = parseInt(document.getElementById("input").value);
+    if (guess == null || isNaN(guess)) {
+        document.getElementById("result").innerHTML = "Invalid entry";
+    } else if (guess < numToFind) {
+        document.getElementById("result").innerHTML = "Your guess is too low";
+    } else if (guess > numToFind) {
+        document.getElementById("result").innerHTML = "Your guess is too high";
+    } else {
+        if (count != 1) {
+            alert("You are the winner :D You found the number after " + count + " tries!");
+        } else {
+            alert("You are the champion!!! You got it at your first try!");
+        }
+        if (confirm("Do you want to play again?")) {
+            document.getElementById("result").innerHTML = "";
+            count = 0;
+        } else {
+            document.getElementById("result").innerHTML = "Thanks for the game!";
+            document.getElementById("count").innerHTML = "";
+            return;
+        }
+    }
+    document.getElementById("count").innerHTML = ("Your guess count is " + count++);
+}
